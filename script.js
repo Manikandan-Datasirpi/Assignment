8919b5239fc9a54e85f8722e04e1c7ba22deb3bc
Manikandan-Datasirpi <manikandan.g@datasirpi.com>
2023-07-13 17:57:59 +0530

    add ts and js file

---
+++ script.js
@@ -0,0 +1,33 @@
+var Fname = "Manikandan";
+console.log(Fname);
+var phno = 8610876521;
+console.log(phno);
+var a = false;
+console.log(a);
+var arr = [6, 3, 8, 6, 8];
+console.log(arr);
+var u1 = {
+    fname: "Manikandan",
+    age: 22,
+    mobile: 8610876521
+};
+console.log(u1);
+;
+var sam = {
+    fname: "Manikandan",
+    age: 22,
+    mobile: 8610876521
+};
+console.log(u1);
+var s = /** @class */ (function () {
+    function s() {
+    }
+    return s;
+}());
+;
+var x = {
+    fname: "Manikandan",
+    age: 22,
+    mobile: 8610876521
+};
+console.log(u1);
---
+++ script.ts
@@ -0,0 +1,50 @@
+let Fname:string="Manikandan";       
+console.log(Fname);
+
+const phno:number = 8610876521;        
+console.log(phno);
+
+const a:boolean = false;            
+console.log(a);
+
+var arr:number[] = [6,3,8,6,8];     
+console.log(arr);
+
+type user={               
+    fname: string;
+    age: number;
+    mobile: number;
+  };
+
+var u1:user={
+    fname:"Manikandan",
+    age:22,
+    mobile:8610876521
+}
+console.log(u1)
+
+interface sam{               
+  fname: String;
+  age: Number;
+  mobile: Number;
+};
+
+var sam:sam={
+  fname:"Manikandan",
+  age:22,
+  mobile:8610876521
+}
+console.log(u1)
+
+class s{               
+  fname: String;
+  age: Number;
+  mobile: Number;
+};
+
+var x:s={
+  fname:"Manikandan",
+  age:22,
+  mobile:8610876521
+}
+console.log(u1)
