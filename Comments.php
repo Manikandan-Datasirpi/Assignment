63c136282cf9f51ff9adfa20904dfee8cfeffdcc
Manikandan-Datasirpi <manikandan.g@datasirpi.com>
2023-07-20 17:24:06 +0530

    Adding Comments program in php

---
+++ Comments.php
@@ -0,0 +1,17 @@
+<?php
+#Single Line Comment
+echo "Welcome to Datasirpi";
+//Another method to declare single line comments
+echo"\n";
+$a=10;
+$b=20;
+$c=$a+$b;
+echo $c;
+/*
+$a=10;
+$b=20;
+$c=$a+$b;
+echo $c;
+Multiline comments
+*/
+?>
