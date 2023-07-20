8b6fea53e39ed2b9f4bc304e2ad56cb8469ec32c
Manikandan-Datasirpi <manikandan.g@datasirpi.com>
2023-07-20 17:14:55 +0530

    Adding Escape sequence program in php

---
+++ Escaping.php
@@ -0,0 +1,24 @@
+<?php
+#New Line - \n
+echo "Welcome to Datasirpi \nI am Manikandan.";
+echo "\n";
+
+#Tab -\t
+echo "Welcome\tto\tDatasirpi";
+echo "\n";
+
+#Escape Single quotes
+echo 'I\'m Manikandan';
+echo"\n";
+
+#Escape Doube quotes
+echo "Ir is \"Apple\"";
+echo "\n";
+
+#carriage return
+echo "I am \rManikandan";
+echo "\n";
+
+#Hex code
+echo "\x65";
+?>
