55439507ae1e0068c34fd1323524ddd8d6763af7
Manikandan-Datasirpi <manikandan.g@datasirpi.com>
2023-07-31 09:20:08 +0530

    Adding Html Form

---
+++ Form/Form.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<title>Registation Form</title>
+</head>
+<body>
+	<header>
+		<h1>
+			Registation Page
+		</h1>
+	</header>
+	<div class="Main">
+		<div class="Box">
+			<form action="Message.php" method="POST" >
+				<table>
+					<tr>
+						<td>
+							Username:
+						</td>
+						<td>
+							<input type="text" name="username">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Email:
+						</td>
+						<td>
+							<input type="email" name="mail">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Password:
+						</td>
+						<td>
+							<input type="Password" name="pass">
+						</td>
+					</tr>
+				</table>
+				<div>
+					<input type="Submit" name="submit" value="Submit">
+					<input type="Button" name="Cancel" value="Cancel">
+				</div>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
---
+++ Form/Message.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Message</title>
+</head>
+<body>
+	<?php
+	if(isset($_POST['submit']))
+	{
+		if(preg_match("/[^A-Z|a-z]/",$_POST['username']))
+		{
+			die("Username is invalid!!");
+		}
+		else if(empty($_POST['pass']))
+		{
+			die("Password is invalid!");
+		}
+		else
+		{
+			$name=$_POST['username'];
+			$mail=$_POST['mail'];
+			$pass=$_POST['pass'];
+			echo "<h1>Hi $name</h1>";
+			echo "<h1>Your email: $mail";
+			echo "<h1>Your password: $pass";
+		}
+	}
+	?>
+</body>
+</html>
